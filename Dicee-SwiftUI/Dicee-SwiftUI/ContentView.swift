565db171baa6614c23a841f8f9ed893e966fba33
Alex Waldron <awaldro1@stevens.edu>
2020-11-21 10:06:40 -0500

    added dicee-SwiftUI

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Alex Waldron on 11/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack{
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(num: leftDiceNumber)
+                    DiceView(num: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                })
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    let num:Int
+    var body: some View {
+        
+        Image("dice\(num)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
